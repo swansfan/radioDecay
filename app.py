337abe4eda8aa2be86d70957c3d4b6e2f2b69ee7
swansfan <45616869+swansfan@users.noreply.github.com>
2020-07-14 19:39:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ marker_colour = np.empty((maxX * maxY, maxTimeSteps + 1), dtype=np.dtype('U7'))
 
 # Run random decay
 atoms, atomsdecayed, atomsremaining = decay.simdecay(maxX, maxY, maxTimeSteps, decayConstant)
+print('original',atomsremaining)
 
 external_stylesheets = [dbc.themes.FLATLY, 'assets/styles.css']
 
@@ -170,7 +171,7 @@ def update_timeseriesgraph(currentstep):
     # xdata = np.arange(len(atomsremaining)).reshape(len(atomsremaining), 1)
     xdata = np.arange(0, currentstep + 1, 1)
     ydata = atomsremaining[range(0, len(atomsremaining))]
-    print(ydata)
+    print('in slider',ydata)
     numatoms = atomsremaining[0]
     numremaining = atomsremaining[currentstep]
     numdecayed = numatoms - numremaining
